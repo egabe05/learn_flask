1309e1a310d8711f0c6bb24e92383bbaaf0448a5
Eric Gaberik <eric.gaberik@cybergrx.com>
2019-03-06 05:35:41 -0700

    initial layout of profile page and navigation link

--- app/routes.py
+++ app/routes.py
@@ -6,6 +6,7 @@ from app import app, db
 from app.forms import LoginForm, RegistrationForm
 from app.models import User
 
+
 @app.route('/')
 @app.route('/index')
 @login_required
@@ -17,6 +18,7 @@ def index():
          'body': 'I am a man cold kind of guy'}
     ]
     return render_template('index.html', title='Home', posts=posts)
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -40,10 +42,12 @@ def login():
         return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
+
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -58,3 +62,14 @@ def register():
         flash('Congratulations, you are now a registered user!')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
