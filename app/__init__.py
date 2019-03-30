f98c38c83a133435d77b7d5c1bbff38955ce9da4
Eric Gaberik <eric.gaberik@cybergrx.com>
2019-03-30 09:28:17 -0600

    implemented email support for password reset

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_login import LoginManager
+from flask_mail import Mail
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 import logging
@@ -14,6 +15,7 @@ db = SQLAlchemy(app)
 login = LoginManager(app)
 login.login_view = 'login'
 migrate = Migrate(app, db)
+mail = Mail(app)
 
 from app import routes, models, errors
 
